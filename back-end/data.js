4f35343a1a610606a2bb67bfaffb9bcbe5a29571
Victor Emanuel <95113352+vitu2@users.noreply.github.com>
2022-12-10 20:33:07 -0300

    Back-end projeto ecommerce

---
+++ back-end/data.js
@@ -0,0 +1,53 @@
+const data = {
+    products: [
+      {
+        "offerName": "Smart Tv Philips 65; 4k Led Android Tv 60hz",
+        "sellerId": 5727,
+        "sellerThumbnail": "https://www.lomadee.com/programas/BR/5727/imagemBox_80x60.png",
+        "offerLink": "https://redir.lomadee.com/v2/3435f15df78",
+        "offerThumbnail": "https://www.girafa.com.br/imagens/Eletronicos/Televisores/smart-tv-philips-65-4k-uhd-led-android-tv-60hz-65pug7406-78-897858-1667854426-8.jpg",
+        "sellerName": "Girafa",
+        "priceTo": 3443.33,
+        "sku": 897858,
+        "categoryName": "Eletrônicos",
+        "categoryId": 1
+      },
+      {
+        "offerName": "Samsung Galaxy S22 Ultra 5g 256 Gb Preto 6.8",
+        "sellerId": 5727,
+        "sellerThumbnail": "https://www.lomadee.com/programas/BR/5727/imagemBox_80x60.png",
+        "offerLink": "https://redir.lomadee.com/v2/34311f8b180",
+        "offerThumbnail": "https://www.girafa.com.br/imagens/Telefonia/Celulares/smartphone-samsung-galaxy-s22-ultra-preto-256gb-12gb-ram-6-8-cam-quad108mp-10mp-12mp-10mp-selfie40mp-898106-1644956802-1.jpg",
+        "sellerName": "Girafa",
+        "priceTo": 7776.67,
+        "sku": 898106,
+        "categoryName": "Conserto de Celulares",
+        "categoryId": 8421
+      },
+      {
+        "offerName": "Smartphone Samsung Galaxy S22 5g 256 Gb 6.1",
+        "sellerId": 5727,
+        "sellerThumbnail": "https://www.lomadee.com/programas/BR/5727/imagemBox_80x60.png",
+        "offerLink": "https://redir.lomadee.com/v2/3431a49ff48",
+        "offerThumbnail": "https://www.girafa.com.br/imagens/Telefonia/Celulares/smartphone-samsung-galaxy-s22-5g-branco-256gb-8gb-ram-6-1-cam-tripla-50mp-10mp-12mp-e-selfie-10mp-898097-1644522733-1.jpg",
+        "sellerName": "Girafa",
+        "priceTo": 6499.9,
+        "sku": 898097,
+        "categoryName": "Conserto de Celulares",
+        "categoryId": 8421
+      },
+      {
+        "offerName": "Smart Tv Samsung 70 4k Tela Sem Limites Visual Livre De Cabos ",
+        "sellerId": 5727,
+        "sellerThumbnail": "https://www.lomadee.com/programas/BR/5727/imagemBox_80x60.png",
+        "offerLink": "https://redir.lomadee.com/v2/343dea3e879",
+        "offerThumbnail": "https://www.girafa.com.br/imagens/Eletronicos/Televisores/smart-tv-samsung-70-uhd-4k-70au7700-processador-crystal-4k-tela-sem-limites-visual-livre-de-cabos-alexa-built-in-controle-unico-893266-1667495224-2.jpg",
+        "sellerName": "Girafa",
+        "priceTo": 5399,
+        "sku": 893266,
+        "categoryName": "Eletrônicos",
+        "categoryId": 1
+      }
+    ],
+  };
+  export default data;
---
+++ back-end/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import data from "./data.js";
+import cors from 'cors'
+const app = express()
+
+app.use(cors())
+
+app.get('/api/products', (req, res) => {
+    res.send(data.products)
+})
+
+app.get('/api/products/slug/:categoryName', (req, res) => {
+    const product = data.products.find(x => x.categoryName === req.params.categoryName)
+
+    if(product){
+        res.send(product)
+    }else{
+        res.status(404).send({message: 'Product Not Found'})
+    }
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Running port ${port}`)
+})
